6aaf21552bc6ef2c46dc2b326f93a6ca2cc374fc
Joel2B <jbuenouas@gmail.com>
2021-10-04 00:05:15 -0600

    Initial commit

---
+++ mdfiles/angular-y-scully.md
@@ -0,0 +1,11 @@
+---
+title: Angular y Scully
+description: 'Cómo construir un blog con Angular y Scully'
+published: true
+---
+
+# Angular y Scully
+Angular es un framework de JavaScript.
+- https://angular.io
+- https://scully.io
+- https://jamstack.org
---
+++ src/app/articles/articles.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ArticlesRoutingModule } from './articles-routing.module';
+import { ArticlesComponent } from './articles.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ArticlesComponent
+  }
+];
+@NgModule({
+  declarations: [
+    ArticlesComponent
+  ],
+  imports: [
+    CommonModule,
+    ArticlesRoutingModule,
+    ArticlesRoutingModule
+  ],
+  exports:[RouterModule]
+})
+export class ArticlesModule { }
